9379dd7f69ff2bbfa2232d1689c860b785b387b1
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-02 01:12:26 +0300

    correct Channel

--- src/channel.py
+++ src/channel.py
@@ -5,22 +5,22 @@ from googleapiclient.discovery import build
 import isodate
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
-print(api_key)
+
 # создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey='AIzaSyAxiXhWOvIei9BgCtEi_vilX-1PT9S5A0I')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
-def printj(dict_to_print: dict) -> None:
-    """Выводит словарь в json-подобном удобном формате с отступами"""
-    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-# AIzaSyAxiXhWOvIei9BgCtEi_vilX-1PT9S5A0I
-
+# channel_id = 'UCMCgOm8GZkHp8zJ6l7_hIuA'
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        info = json.dumps(self.channel, indent=2, ensure_ascii=False)
+        return print(info)
+
