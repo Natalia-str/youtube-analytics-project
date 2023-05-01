b6291ceb15c77aa5363c8530615af474cdc47e93
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-01 23:37:29 +0300

    add pyproject.toml

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,25 @@
+import json
+import os
+from googleapiclient.discovery import build
 
+import isodate
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+print(api_key)
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey='AIzaSyAxiXhWOvIei9BgCtEi_vilX-1PT9S5A0I')
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+# AIzaSyAxiXhWOvIei9BgCtEi_vilX-1PT9S5A0I
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
