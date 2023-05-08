41c5d1f61a6fe87f20a805021352e47f3cebdaed
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-08 22:20:52 +0300

    correct Channel add id канала название канала описание канала ссылка на канал количество подписчиков количество видео общее количество просмотров

--- src/channel.py
+++ src/channel.py
@@ -15,8 +15,45 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+
+    @property
+    def channel_id(self):
+        """ID канала"""
+        return self.__channel_id
+
+    @property
+    def title(self):
+        """название канала"""
+        return self.channel["items"][0]["snippet"]["title"]
+
+    @property
+    def description(self):
+        """описание канала"""
+        return self.channel["items"][0]["snippet"]["description"]
+
+    @property
+    def url(self):
+        """ссылка на канал"""
+        return f"https://www.youtube.com/channel/{self.__channel_id}"
+
+
+    @property
+    def subscriberCount(self):
+        """количество подписчиков"""
+        return self.channel["items"][0]["statistics"]["subscriberCount"]
+
+    @property
+    def viewCount(self):
+        """общее количество просмотров"""
+        return self.channel["items"][0]["statistics"]["viewCount"]
+
+    @property
+    def videoCount(self):
+        """количество видео"""
+        return self.channel["items"][0]["statistics"]["videoCount"]
 
 
     def print_info(self) -> None:
@@ -24,3 +61,8 @@ class Channel:
         info = json.dumps(self.channel, indent=2, ensure_ascii=False)
         return print(info)
 
+vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
+print(vdud.title)
+print(vdud.channel_id)
+print(vdud.url)
+print(vdud.videoCount)
