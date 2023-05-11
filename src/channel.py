d7cbc416ed75ccd330a76adff30316271e7239b5
Natalia Stroganova <ntstroganova@gmail.com>
2023-05-12 01:13:12 +0300

    correct channel

--- src/channel.py
+++ src/channel.py
@@ -18,63 +18,74 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
         self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-
+        self.title = self.channel["items"][0]["snippet"]["title"]
+        self.description = self.channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+        self.subscriber_count = self.channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
+        self.view_count = self.channel["items"][0]["statistics"]["videoCount"]
 
     @property
     def channel_id(self):
         """ID канала"""
         return self.__channel_id
 
-    @property
-    def title(self):
-        """название канала"""
-        return self.channel["items"][0]["snippet"]["title"]
-
-    @property
-    def description(self):
-        """описание канала"""
-        return self.channel["items"][0]["snippet"]["description"]
-
-    @property
-    def url(self):
-        """ссылка на канал"""
-        return f"https://www.youtube.com/channel/{self.__channel_id}"
-
-
-    @property
-    def subscriber_count(self):
-        """количество подписчиков"""
-        return self.channel["items"][0]["statistics"]["subscriberCount"]
-
-    @property
-    def view_count(self):
-        """общее количество просмотров"""
-        return self.channel["items"][0]["statistics"]["viewCount"]
-
-    @property
-    def video_count(self):
-        """количество видео"""
-        return self.channel["items"][0]["statistics"]["videoCount"]
+    # @property
+    # def title(self):
+    #     """название канала"""
+    #     return self.channel["items"][0]["snippet"]["title"]
+    #
+    # @property
+    # def description(self):
+    #     """описание канала"""
+    #     return self.channel["items"][0]["snippet"]["description"]
+    #
+    # @property
+    # def url(self):
+    #     """ссылка на канал"""
+    #     return f"https://www.youtube.com/channel/{self.__channel_id}"
+    #
+    #
+    # @property
+    # def subscriber_count(self):
+    #     """количество подписчиков"""
+    #     return self.channel["items"][0]["statistics"]["subscriberCount"]
+    #
+    # @property
+    # def view_count(self):
+    #     """общее количество просмотров"""
+    #     return self.channel["items"][0]["statistics"]["viewCount"]
+    #
+    # @property
+    # def video_count(self):
+    #     """количество видео"""
+    #     return self.channel["items"][0]["statistics"]["videoCount"]
 
     @classmethod
     def get_service(cls):
-        response = requests.get("https://developers.google.com/youtube/v3/docs/channels/list?hl=ru#http-request")
-        print(response.status_code)
-        print(response.content)
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    # def to_json(self):
+    #     """сохраняет в файл значения атрибутов экземпляра Channel"""
+    #     info = {'ID канала': self.__channel_id,
+    #              'название канала': self.title,
+    #              'описание канала': self.description,
+    #              'ссылка на канал': self.url,
+    #              'количество подписчиков': self.subscriber_count,
+    #              'общее количество просмотров': self.view_count,
+    #              'количество видео': self.video_count}
+    #
+    #     with open('data_channel.json', 'w', encoding='utf-8') as f:
+    #         json.dump(info, f, indent=2, ensure_ascii=False)
 
     def to_json(self):
         """сохраняет в файл значения атрибутов экземпляра Channel"""
-        info = {'ID канала': self.__channel_id,
-                 'название канала': self.title,
-                 'описание канала': self.description,
-                 'ссылка на канал': self.url,
-                 'количество подписчиков': self.subscriber_count,
-                 'общее количество просмотров': self.view_count,
-                 'количество видео': self.video_count}
-
+        info = self.__dict__
         with open('data_channel.json', 'w', encoding='utf-8') as f:
-            json.dump(info, f, indent=2, ensure_ascii=False)
 
+            json.dump(info, f, indent=2, ensure_ascii=False)
 
 
 
